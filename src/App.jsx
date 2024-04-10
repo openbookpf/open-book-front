9384c7ac36ca0d0d4d871e698f581486aaa0c920
KJoaquinklein <joa_klein1@hotmail.com>
2024-04-10 12:52:41 -0300

    tailwind config 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './views/Home/Home';
-import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold underline">
-        <NavBar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-        </Routes>
-    </div>
-  )
+    return (
+        <div className="text-3xl font-poppinds text-orange-0 mx-5">
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
