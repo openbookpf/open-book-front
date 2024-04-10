7402d5725a48b3f3080099948fbbb7c817f59751
Daiana Carabetta <dai_dvc@hotmail.com>
2024-04-10 12:31:20 -0300

    componentes2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="text-3xl font-bold underline">
-        {/* <NavBar/> */}
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
