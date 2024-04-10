591b8ac07e1d5db858b77814224f7ed4a97ffa5b
Lucas Fraggioli <75335331+lfraggioli@users.noreply.github.com>
2024-04-10 00:01:40 -0300

    Revert "landing initial commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import Landing from './views/Landing/Landing';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <Routes>
-        <Route path='/' element={<Landing/>}/>
-      </Routes>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
