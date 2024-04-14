0551970c60d06f90340be3d4e5bfd8c3198d450c
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-04-14 17:50:21 -0300

    filter by genre fixed

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,42 +1,39 @@
+// reducer.js
+import { FILTER_BOOKS_BY_GENRE, GET_BOOKS } from "./actions";
+
 const initialState = {
-    books: [],
-    booksFilter: [],
+  books: [], // Suponiendo que tienes una lista inicial de libros
+  filteredBooks: [],
 };
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "GET_BOOKS":
-            return {
-                ...state,
-                books: action.payload,
-                booksFilter: action.payload,
-            };
+function booksReducer(state = initialState, action) {
+  switch (action.type) {
+    case FILTER_BOOKS_BY_GENRE:
+      // return {
+      //   ...state,
+      //   filteredBooks: state.books.filter(
+      //     (book) => book.genre === action.payload
+      //   ),
+      // };
+      const filtered = [...state.books].filter((book) => {
+        // Verificamos si book.genre es un array y contiene action.payload
+        return book.genre.includes(action.payload);
+      });
+      return {
+        ...state,
+        filteredBooks: filtered,
+      };
 
-        case "GET_BOOKS_FILTER_AUTHOR":
-            if (action.payload === "All") {
-                return { ...state, booksFilter: [...state.books] };
-            }
-            return { ...state, booksFilter: action.payload };
+    case GET_BOOKS:
+      return {
+        ...state,
+        books: action.payload,
+        filteredBooks: action.payload,
+      };
 
-        case "GET_BOOKS_FILTER_GENERO":
-            if (action.payload === "All") {
-                return { ...state, booksFilter: [...state.books] };
-            }
-            return { ...state, booksFilter: action.payload };
+    default:
+      return state;
+  }
+}
 
-        case "GET_BOOKS_FILTER_PRICE":
-            return {
-                ...state,
-                booksFilter: state.books.filter(
-                    (book) =>
-                        (action.payload.min === "" || book.price >= action.payload.min) &&
-                        (action.payload.max === "" || book.price <= action.payload.max)
-                ),
-            };
-
-        default:
-            return { ...state };
-    }
-};
-
-export default reducer;
+export default booksReducer;
