7ed7428bc98f6f085bc5d5c02dd5ec332e0dc278
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-04-15 20:02:36 -0300

    array de libros actualizado

---
+++ src/data/arrayBestSellers.js
@@ -0,0 +1,203 @@
+export default [
+  {
+    ISBN: "9789875666474",
+    book_title: "Ficciones",
+    author: "Jorge Luis Borges",
+    book_cover_url:
+      "https://data.livriz.com/media/MediaSpace/F9AFB48D-741D-4834-B760-F59344EEFF34/45/9f4383e7-acc8-40fa-a831-443091292413/46fc0098-071a-4a9e-8d19-dc5e7eb9e602.jpg",
+    genre: "Fiction",
+    book_description:
+      'Ficciones es quizá el libro más famoso de Jorge Luis Borges. En él se encuentran algunos de sus cuentos más conocidos, como "Tlön, Uqbar, Orbis Tertius", "Pierre Menard, autor del Quijote", "Funes el memorioso", "La lotería en Babilonia", "El jardín de los senderos que se bifurcan" o "La biblioteca de Babel".',
+    price: 15.99,
+  },
+  {
+    ISBN: "9788433998859",
+    book_title: "Nuestra Parte de Noche",
+    author: "Mariana Enriquez",
+    book_cover_url:
+      "https://data.livriz.com/media/MediaSpace/F9AFB48D-741D-4834-B760-F59344EEFF34/45/051d14ff-58cb-4538-8008-57500c041a8f/a7e12dc1-e7f7-4d35-89b3-0e73b7885354.jpg",
+    genre: "Horror",
+    book_description:
+      "Un padre y un hijo atraviesan Argentina por carretera, desde Buenos Aires hacia las cataratas de Iguazú, en la frontera norte con Brasil. ",
+    price: 20,
+  },
+
+  {
+    ISBN: "9788418395635",
+    book_title: "Robinson Crusoe Td",
+    author: "Defoe, Daniel",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/3a42fcf346f04b514de4707bb1590a91.jpg",
+    genre: "Adventure",
+    book_description:
+      "Tras una travesía llena de aventuras y peligros, el marinero Robinson Crusoe naufraga en una isla desierta donde le esperan más de dos décadas de absoluta soledad.",
+    price: 30.0,
+  },
+  {
+    ISBN: "9780545139700",
+    book_title: "Harry Potter and the Deathly Hallows (Book 7)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/c6bbdfe21440918be26f9b7f15a135c4.jpg",
+    genre: "Fantasy",
+    book_description:
+      "It's no longer safe for Harry at Hogwarts, so he and his best friends, Ron and Hermione, are on the run. Professor Dumbledore has given them clues about what they need to do to defeat the dark wizard, Lord Voldemort, once and for all, but it's up to them to figure out what these hints and suggestions really mean.",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780439064873",
+    book_title: "Harry Potter And The Chamber Of Secrets (Book 2)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/b873fa86ec27ba3556e692be1a7cb9de.jpg",
+    genre: "Fantasy",
+    book_description:
+      "The Dursleys were so mean and hideous that summer that all Harry Potter wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. But just as he's packing his bags, Harry receives a warning from a strange, impish creature named Dobby who says that if Harry Potter returns to Hogwarts, disaster will strike.And strike it does.",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780590353427",
+    book_title: "Harry Potter And The Sorcerer´s Stone (Book 1)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/68ee3669135dccf2358b8bcb72c803b8.jpg",
+    genre: "Fantasy",
+    book_description:
+      "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invisibility. All he knows is a miserable life with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley - a great big swollen spoiled bully. Harry's room is a tiny closet at the foot of the stairs, and he hasn't had a birthday party in eleven years",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780439358071",
+    book_title: "Harry Potter And The Order Of The Phoenix (Book 5)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/c246726a594105e3c66cfdf68325fc76.jpg",
+    genre: "Fantasy",
+    book_description:
+      "In his fifth year at Hogwart's, Harry faces challenges at every turn, from the dark threat of He-Who-Must-Not-Be- Named and the unreliability of the government of the magical world to the rise of Ron Weasley as the keeper of the Gryffindor Quidditch Team. Along the way he learns about the strength of his friends, the fierceness of his enemies, and the meaning of sacrifice",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780439785969",
+    book_title: "Harry Potter And The Half Blood Prince (Book 6)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/35ceabaa28765a3d977fcfbab03493c4.jpg",
+    genre: "Fantasy",
+    book_description:
+      "In a brief statement on Friday night, Minister for Magic Cornelius Fudge confirmed that He Who Must Not Be Named has returned to this country and is once more active. It is with great regret that I must confirm that the wizard styling himself Lord - well, you know who I mean - is alive and among us again, said Fudge.' These dramatic words appeared in the final pages of Harry Potter and the Order of the Phoenix. In the midst of this battle of good and evil, Harry Potter and the Half-Blood Prince takes up the story of Harry Potter's sixth year at Hogwarts School of Witchcraft and Wizardry, with Voldemort's power and followers increasing day by day",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780439136365",
+    book_title: "Harry Potter And The Prisoner Of Azkaban (Book 3)",
+    author: "J. K. Rowling",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/8bd2a103ee5b0ac751eb1053ef3c7014.jpg",
+    genre: "Fantasy",
+    book_description:
+      "An exciting caldron of danger and intrigue awaits your child in this installment of J. K. Rowling's now-classic series. Harry is only entering his third year at the Hogwarts School, but his magical powers will soon be put to the test. A notorious prisoner has escaped -- and all signs indicate he's headed for Hogwarts to settle an old score. Will Harry be up to the challenge?",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780061122415",
+    book_title: "1984",
+    author: "George Orwell",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/61ZewDE3beL._SL1200_.jpg",
+    genre: "Fiction",
+    book_description:
+      "Winston Smith wrestles with oppression in Oceania, a place where the Party scrutinizes human actions with ever-watchful Big Brother. Defying a ban on individuality, Winston dares to express his thoughts in a diary and pursues a relationship with Julia. These criminal deeds bring Winston into the eye of the opposition, who then must reform the nonconformist.",
+    price: 14.99,
+  },
+
+  {
+    ISBN: "9780141439563",
+    book_title: "Pride and Prejudice",
+    author: "Jane Austen",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/81NLDvyAHrL._SL1500_.jpg",
+    genre: "Fiction",
+    book_description:
+      "Pride and Prejudice, Jane Austen's witty comedy of manners—one of the most popular novels of all time—features splendidly civilized sparring between the proud Mr. Darcy and the prejudiced Elizabeth Bennet as they play out their spirited courtship in a series of eighteenth-century drawing-room intrigues.",
+    price: 9.99,
+  },
+
+  {
+    ISBN: "9780141439723",
+    book_title: "Moby-Dick",
+    author: "Herman Melville",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/81+3Wl6KVNL._SL1500_.jpg",
+    genre: "Fiction",
+    book_description:
+      "The story of the voyage of the whaling ship Pequod, commanded by Captain Ahab, who leads his crew on a hunt for the great whale Moby Dick, driven by his obsession to extract revenge on the creature that destroyed his last ship and left him crippled.",
+    price: 13.99,
+  },
+  {
+    ISBN: "9781984823489",
+    book_title: "The Silent Patient",
+    author: "Alex Michaelides",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/91lslnZ-btL._SL1500_.jpg",
+    genre: "Thriller",
+    book_description:
+      "The Silent Patient is a shocking psychological thriller of a woman's act of violence against her husband and of the therapist obsessed with uncovering her motive.",
+    price: 16.99,
+  },
+  {
+    ISBN: "9780593099322",
+    book_title: "Dune",
+    author: "Frank Herbert",
+    book_cover_url:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg",
+    genre: "Science Fiction",
+    book_description:
+      "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for...",
+    price: 16.99,
+  },
+  {
+    ISBN: "9788445071403",
+    book_title: "The Fellowship of the Ring (The Lord of the Rings #1)",
+    author: "J.R.R. Tolkien",
+    book_cover_url:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1704737084i/222947.jpg",
+    genre: "Fantasy",
+    book_description:
+      "One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them. In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell into the hands of Bilbo Baggins, as told in The Hobbit. In a sleepy village in the Shire, young Frodo Baggins finds himself faced with an immense task, as his elderly cousin Bilbo entrusts the Ring to his care. Frodo must leave his home and make a perilous journey across Middle-earth to the Cracks of Doom, there to destroy the Ring and foil the Dark Lord in his evil purpose.",
+    price: 8.99,
+  },
+  {
+    ISBN: "9786073908245",
+    book_title: "The Two Towers (The Lord of the Rings #2)",
+    author: "J.R.R. Tolkien",
+    book_cover_url:
+      "https://tmgames.com/ins/images/stories/virtuemart/product/lotr%202.jpg",
+    genre: "Fantasy",
+    book_description:
+      "Frodo and his Companions of the Ring have been beset by danger during their quest to prevent the Ruling Ring from falling into the hands of the Dark Lord by destroying it in the Cracks of Doom. They have lost the wizard, Gandalf, in a battle in the Mines of Moria. And Boromir, seduced by the power of the Ring, tried to seize it by force. While Frodo and Sam made their escape, the rest of the company was attacked by Orcs. Now they continue the journey alone down the great River Anduin—alone, that is, save for the mysterious creeping figure that follows wherever they go.",
+    price: 8.99,
+  },
+  {
+    ISBN: "9788445075753",
+    book_title: "Return of the King (The Lord of the Rings #3)",
+    author: "J.R.R. Tolkien",
+    book_cover_url:
+      "https://tmgames.com/ins/images/stories/virtuemart/product/lotr%203.jpg",
+    genre: "Fantasy",
+    book_description:
+      "The Dark Lord has risen, and as he unleashes hordes of Orcs to conquer all Middle-earth, Frodo and Sam struggle deep into his realm in Mordor. To defeat Sauron, the One Ring must be destroyed in the fires of Mount Doom. But the way is impossibly hard, and Frodo is weakening. The Ring corrupts all who bear it and Frodo’s time is running out. Will Sam and Frodo succeed, or will the Dark Lord rule Middle-earth once more?",
+    price: 8.99,
+  },
+  {
+    ISBN: "9789505470631",
+    book_title: "The Hobbit (The Lord of the Rings #0)",
+    author: "J.R.R. Tolkien",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/91U3Yg8PFDL._SL1500_.jpg",
+    genre: "Fantasy",
+    book_description:
+      "Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure. They have launched a plot to raid the treasure hoard guarded by Smaug the Magnificent, a large and very dangerous dragon.",
+    price: 7.99,
+  },
+];
