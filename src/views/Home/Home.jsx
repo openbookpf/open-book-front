7d69d533aec8662f00bba8b3526f8701ee0e9686
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-04-15 13:32:16 -0300

    changes

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,10 +4,21 @@ const Home = () => {
   return (
     <>
       <div>
-        <div className="font-bold text-black">
-          <h1>Logo</h1>
+        <div className="mt-16 text-black"></div>
+        <div className="justify-center bg-black items-center align-middle h-96 relative">
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundImage:
+                "url(https://img.freepik.com/foto-gratis/abundante-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29660.jpg)",
+              backgroundSize: "contain",
+              opacity: 0.5,
+            }}
+          ></div>
+          <h3 className="z-30 mx-auto py-32 font-bold text-white-0 text-center relative">
+            Welcome to OpenBook, your definitive literary destination
+          </h3>
         </div>
-        {/* <Cards/> */}
       </div>
       <footer>
         <div>
