6a3e2b495c326cd4126d7b6e555d281570a642c3
KJoaquinklein <joa_klein1@hotmail.com>
2024-04-15 17:44:08 -0300

    Validaciones Form Create

---
+++ src/views/CreateBook/validationsFormCreate.js
@@ -0,0 +1,94 @@
+export const validationsForm = (key, value, errorForm, setErrorForm) => {
+    //?-validacion del ISBN -------------------------------------------------------
+    if (key === "ISBN") {
+        if (!value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (isNaN(value)) {
+            return setErrorForm({ ...errorForm, [key]: "The ISBN must be a number" });
+        }
+        if (value < 1 || value > 9999999999999) {
+            return setErrorForm({ ...errorForm, [key]: "Incorrect ISBN" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+
+    //?-validacion del titulo------------------------------------------------------
+    if (key === "book_title") {
+        if (!value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (value.length > 255) {
+            return setErrorForm({ ...errorForm, [key]: "The title is too long" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+
+    //?-validacion del autor-------------------------------------------------------
+    if (key === "author") {
+        if (!value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (/\d/.test(value)) {
+            return setErrorForm({ ...errorForm, [key]: "The author's name cannot contain numbers." });
+        }
+        if (value.length === 1) {
+            return setErrorForm({ ...errorForm, [key]: "Author's name is too short" });
+        }
+        if (value.length > 100) {
+            return setErrorForm({ ...errorForm, [key]: "Author's name is too long" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+
+    //?-validacion del genero------------------------------------------------------
+    if (key === "genre") {
+        if (!value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (/\d/.test(value)) {
+            return setErrorForm({ ...errorForm, [key]: "Gender cannot contain numbers" });
+        }
+        if (value.length === 1) {
+            return setErrorForm({ ...errorForm, [key]: "The genre is too short" });
+        }
+        if (value.length > 100) {
+            return setErrorForm({ ...errorForm, [key]: "The genre is too long" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+
+    //?-validacion de la descripcion-----------------------------------------------
+    if (key === "book_description") {
+        if (!value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (value.length === 1) {
+            return setErrorForm({ ...errorForm, [key]: "The description is too short" });
+        }
+        if (value.length > 2000) {
+            return setErrorForm({ ...errorForm, [key]: "The description is too long" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+
+    //?-validacion del precio------------------------------------------------------
+    if (key === "price") {
+        if ((value <= 0) | !value) {
+            return setErrorForm({ ...errorForm, [key]: "This field is required" });
+        }
+        if (Number.isInteger(Number(value))) {
+            return setErrorForm({ ...errorForm, [key]: "The price must include cents (10.00, 8.99, ...)" });
+        }
+        if (value > 9999) {
+            return setErrorForm({ ...errorForm, [key]: "The price is too high" });
+        }
+        return setErrorForm({ ...errorForm, [key]: "" });
+    }
+};
+
+export const validationImg = (selectedFile, errorForm, setErrorForm) => {
+    if (!selectedFile) {
+        setErrorForm({ ...errorForm, img: "This field is required" });
+    }
+};
