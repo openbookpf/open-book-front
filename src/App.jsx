2876269a0f73689554d64bbf218b7243a82cc743
Daiana Carabetta <dai_dvc@hotmail.com>
2024-04-15 12:21:54 -0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import NavBar from "./components/NavBar/NavBar";
 import BookList from "./views/List/BookList";
 import Detail from "./views/Detail/Detail";
 import About from "./components/About/About";
-import Cards from "./components/Cards/Cards";
+import arrayLibros from "./data/arrayLibros";
 
 function App() {
   return (
     <div className="App flex flex-col text-3xl font-poppins">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/books" element={<Cards />} /> */}
+        <Route path="/" element={<Home books={arrayLibros} />} />
         <Route path="/aboutus" element={<About />} />
         <Route path="/books" element={<BookList />} />
         <Route path="/detail/:isbn" element={<Detail />} />
