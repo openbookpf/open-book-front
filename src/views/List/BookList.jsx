2f221f298ec15a9efa6c38c024d8d61480ece708
GFernandeFleitas <derlis.fernandez15@gmail.com>
2024-04-23 19:48:38 -0400

    fixed styles

--- src/views/List/BookList.jsx
+++ src/views/List/BookList.jsx
@@ -92,39 +92,41 @@ const BookList = () => {
 
         <div className="flex flex-col basis-11/12 w-full ml-10">
           <div className="flex flex-col h-10 items-center justify-items-end">
-            <div className="flex items-center">
-              {filters.map((filt, index) =>
-                filt ? (
-                  <div
-                    className="flex items-center text-sm  bg-orange-0 bg-opacity-30 h-10 mx-2 px-2 rounded-xl duration-200 hover:scale-105"
-                    key={filt}
-                  >
-                    <p className="mr-2 text-lg">
-                      {!isNaN(filt) ? <>$</> : null}
-                      {filt}
-                    </p>
-                    <button
-                      onClick={() => handleCloseFilter(filt)}
-                      className="duration-200 hover:bg-orange-0 rounded-full w-5 h-5 flex justify-center items-center"
+            <div className="flex flex-row justify-between w-full">
+              <div className="flex items-center">
+                {filters.map((filt, index) =>
+                  filt ? (
+                    <div
+                      className="flex items-center text-sm  bg-orange-0 bg-opacity-30 h-10 mx-2 px-2 rounded-xl duration-200 hover:scale-105"
+                      key={filt}
                     >
-                      <IoMdClose />
-                    </button>
-                  </div>
-                ) : (
-                  <div key={index}></div>
-                )
-              )}
-            </div>
-            <div className="flex items-center justify-end bg-orange-0 bg-opacity-30 px-2 py-1 rounded-xl ml-auto">
-              <div className="text-lg flex justify-end ">
-                <p className="pr-2">Sort by:</p>
-                <select onChange={handleSortChange} className="rounded-xl">
-                  <option value="">None</option>
-                  <option value="title-asc">Title (A - Z)</option>
-                  <option value="title-desc">Title (Z - A)</option>
-                  <option value="price-min">Price (Low - High)</option>
-                  <option value="price-max">Price (High - Low)</option>
-                </select>
+                      <p className="mr-2 text-lg">
+                        {!isNaN(filt) ? <>$</> : null}
+                        {filt}
+                      </p>
+                      <button
+                        onClick={() => handleCloseFilter(filt)}
+                        className="duration-200 hover:bg-orange-0 rounded-full w-5 h-5 flex justify-center items-center"
+                      >
+                        <IoMdClose />
+                      </button>
+                    </div>
+                  ) : (
+                    <div key={index}></div>
+                  )
+                )}
+              </div>
+              <div className="flex items-center justify-end bg-orange-0 bg-opacity-30 px-2 py-1 rounded-xl ml-auto">
+                <div className="text-lg flex justify-end ">
+                  <p className="pr-2">Sort by:</p>
+                  <select onChange={handleSortChange} className="rounded-xl">
+                    <option value="">None</option>
+                    <option value="title-asc">Title (A - Z)</option>
+                    <option value="title-desc">Title (Z - A)</option>
+                    <option value="price-min">Price (Low - High)</option>
+                    <option value="price-max">Price (High - Low)</option>
+                  </select>
+                </div>
               </div>
             </div>
           </div>
