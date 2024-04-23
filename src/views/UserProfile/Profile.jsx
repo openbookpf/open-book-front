0c1fa9949c1e02e65e0c3ac6b2d165098bfeaa61
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-04-23 15:17:05 -0300

    validation for admin added

--- src/views/UserProfile/Profile.jsx
+++ src/views/UserProfile/Profile.jsx
@@ -1,26 +1,28 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
   if (!isAuthenticated) {
     return (
       <div className="text-center flex flex-col mt-20">
-        <p>Por favor inicia sesión</p>
+        <p>Por favor, inicia sesión</p>
       </div>
     );
   }
 
   return (
-    <div className="text-center flex flex-col mt-20">
-      <h2 className="text-3xl">{user.name}</h2>
-      <p>{user.email}</p>
-      <img className="w-20" src={user.picture} alt={user.name} />
+    <div className="text-white-0 bg-blue-0 h-auto w-72 mt-20 mx-auto rounded-lg shadow-md">
+      <div className="flex flex-col text-center mx-auto py-14">
+        <img
+          className="w-36 mx-auto rounded-full"
+          src={user.picture}
+          alt={user.name}
+        />
+        <h2 className="text-xl font-semibold">{user.name}</h2>
+        <p className="text-sm font-light">{user.email}</p>
+      </div>
     </div>
   );
 };
