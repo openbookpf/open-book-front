c1184dcc7f165575a90024209c66e9fe30f157bd
Daniel <danielmasmar@gmail.com>
2024-04-30 19:52:41 -0400

    favorites

--- src/views/EditProfile/EditProfile.jsx
+++ src/views/EditProfile/EditProfile.jsx
@@ -7,6 +7,16 @@ const EditProfile = (props) => {
     email_address: "",
     picture: "",
   });
+  function submitear(e){
+    e.preventDefault()
+    fetch("http://localhost:3001/user",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuario),
+    })
+  }
   useEffect(() => {
     fetch(`http://localhost:3001/user/${props.newuser}`)
       .then((res) => res.json())
@@ -14,9 +24,18 @@ const EditProfile = (props) => {
   }, []);
   return (
     <div>
-      <h2>edicion de usuario</h2>
-      <label htmlFor="name">name</label>
-      <input name="name" value={userobject} />
+      <form onSubmit={submitear}>
+        <h2>edicion de usuario</h2>
+        <label htmlFor="name">name</label>
+        <input name="name" value={usuario.user_name} defaultValue={userobject.user_name}/>
+
+        <label htmlFor="name">email</label>
+        <input name="name" value={usuario.email_address} defaultValue={userobject.email_address}/>
+
+        <label htmlFor="name">picture</label>
+        <input name="name" value={usuario.picture} defaultValue={userobject.picture}/>
+        {/* <button onClick={}>edit</button> */}
+      </form>
     </div>
   );
 };
